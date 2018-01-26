4573ec303951c063f2eac192ec32213c418c3049
Trickart <richard.sandeau.pro@gmail.com>
2018-01-26 15:05:01 +0100

    programme menu (bouton)

--- programme_arduino/menu_lcd/menu_lcd.ino
+++ programme_arduino/menu_lcd/menu_lcd.ino
@@ -1,38 +1,33 @@
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-const int buttonPin = 10;     
-const int buttonPin2 = 6;     
-  int buttonState = 0;         
-int buttonState2 = 0;
-   void setup() {
-     lcd.begin(16, 2);
-     lcd.print("TEST");   
+#include <Wire.h>
+#include<LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 20, 2);
+
+int buttonState = 0;
+const int button = 24;
+const int led = 26;                   
+
+void setup() {
+pinMode(button, INPUT);
+pinMode(led, OUTPUT);
+lcd.init();  // initialisation de l'afficheur
+lcd.print("TEST");   
    }
-  void loop() {   
-buttonState = digitalRead(buttonPin);
-buttonState2 = digitalRead(buttonPin2);
-pinMode(buttonPin, INPUT);
-pinMode(buttonPin2, INPUT);
-if (buttonState == HIGH) {
-  delay(500); //on évite de sauter les menus d'un coup
+   
+void loop() {   
+  
+buttonState = digitalRead(button);
+if (buttonState == LOW) {
+  digitalWrite(led, HIGH);
+  lcd.clear(); 
+  lcd.print("Pulp Fiction");   
+
+digitalWrite(led, LOW);
+buttonState = digitalRead(button);
+if (buttonState == LOW) {
+  digitalWrite(led, HIGH);
    lcd.clear(); 
-  lcd.print("menu #1");/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    if (buttonState2 == HIGH){
-      delay(300);
-      lcd.clear();
-      lcd.print("SOUS menu 1");   
-buttonState = digitalRead(buttonPin);
-buttonState2 = digitalRead(buttonPin2);
-if (buttonState == HIGH) {
-  delay(500); //on évite de sauter les menus d'un coup
-   lcd.clear(); 
-   lcd.print("menu #2");
-  if (buttonState2 == HIGH){
-      delay(500);
-      lcd.clear();
-      lcd.print("SOUS menu 2");   
-  }
-  }
+   lcd.print("Kill Bill");  
   }
   }
   }
